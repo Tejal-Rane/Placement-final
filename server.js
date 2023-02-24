72f8f06d5738c6317f179e490bc13cfb5299d68b
Tejal-Rane <87368902+Tejal-Rane@users.noreply.github.com>
2023-02-24 22:21:37 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,70 @@
+const express= require("express");
+const dotenv= require("dotenv");
+const connectDB= require("./config/db.js");
+const authRoutes= require("./routes/authRoute.js");
+const adminRoutes= require("./routes/adminRoutes.js");
+const userRoutes= require("./routes/userRoutes.js");
+const cors= require("cors");
+const cookieParser= require("cookie-parser");
+const nodemailer= require("nodemailer");
+const hbs= require("nodemailer-express-handlebars");
+const path= require("path");
+const forumRoutes= require("./routes/forumRoute.js");
+
+//configure env
+dotenv.config();
+
+
+//rest object
+const app = express();
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+//middelwares
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
+app.use(cookieParser());
+
+//routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/admin", adminRoutes);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/", forumRoutes);
+var transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.MAIL_USERNAME,
+    pass: process.env.MAIL_PASSWORD,
+  },
+});
+
+transporter.use(
+  "compile",
+  hbs({
+    viewEngine: {
+      extname: ".hbs",
+      partialsDir: path.resolve(`./views`),
+      defaultLayout: false,
+    },
+    viewPath: path.resolve("./views"),
+    extName: ".hbs",
+  })
+);
+
+//rest api
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to ecommerce app</h1>");
+});
+
+
+const port = process.env.PORT || 8080;
+//listen port
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log(`Server Running in on port ${port}`);
+  });
+});
+module.exports=transporter
